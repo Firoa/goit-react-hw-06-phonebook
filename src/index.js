7a05e490813d3c5e51a7464e4b91af50a6566708
Sasha <in200pac1lab@gmail.com>
2020-05-02 17:31:51 +0300

    rework small msts

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from './containers/AppContainer';
 import './index.css'
 import  store  from "./redux/store";
 import { Provider } from "react-redux";
-console.log(store);
+
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
